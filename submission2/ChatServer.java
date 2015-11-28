064673b0267453f347f00ac32e853e6b6c532dcf
DCUDobby <daire.obruachail2@mail.dcu.ie>
2015-11-28 16:00:12 +0000

    Added more stuff, finished buffer class, error with thread creation/control has arisen

--- submission2/ChatServer.java
+++ submission2/ChatServer.java
@@ -1,18 +1,47 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 class Buffer{
-	private boolean roomAvailable;
-	private boolean dataAvailable;
-	private String[][] buffer = new String[10][];
+	private boolean roomAvailable = true;
+	private boolean dataAvailable = false;
+	private int occupied = 0;
+	private int size;
+	private int nextIn = 0;
+	private int nextOut = 0;
+	private String[] buffer;
 	
-	public Buffer(){
-		
+	public Buffer(int size){
+		this.size = size;
+		buffer = new String[size];
 	}
 	
-	public add(){
-		
+	public synchronized void add(String data) throws InterruptedException{
+		while(!roomAvailable)
+			wait();
+		buffer[nextIn] = data;
+		nextIn = (nextIn + 1)%size;
+		dataAvailable = true;
+		occupied++;
+		if(occupied == size)
+			roomAvailable = false;
+		notifyAll();
 	}
 	
-	public remove(){
-		
+	public synchronized String remove() throws InterruptedException{
+		while(!dataAvailable)
+			wait();
+		String data = buffer[nextOut];
+		nextOut = (nextOut + 1)%size;
+		roomAvailable = true;
+		occupied--;
+		if(occupied == 0)
+			dataAvailable = false;
+		notifyAll();
+		return data;
 	}
 }
 
@@ -26,11 +55,13 @@ class ServerThread implements Runnable{
 	public ServerThread(Socket socket, Buffer buffer){ //this is a producer
 		this.socket = socket;
 		this.buffer = buffer;
-		out = new PrintWriter(socket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		try{
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		}catch(IOException b){System.out.println("Error creating printwriter or bufferedreader");}
 	}
 	
-	public write(String output){
+	public void write(String output){
 		out.println(output);
 	}
 	
@@ -41,29 +72,38 @@ class ServerThread implements Runnable{
 
 			while ((inputLine = in.readLine()) != null) {
 				buffer.add(inputLine);
-				if (inLine.equals("Quit"))
+				if (inputLine.equals("Quit"))
 					break;
 			}
-		} catch(IOException e) {System.out.println("Error running serverthread");}
+		} catch(IOException|InterruptedException e) {System.out.println("Error running serverthread");}
 	}
 }
 
 class Consumer implements Runnable{
 	private Buffer buffer;
+	private ArrayList<ServerThread> threadList;
+	boolean running = true;
 	
-	public Consumer(Buffer buffer){
+	public Consumer(Buffer buffer, ArrayList<ServerThread> threadList){
 		this.buffer = buffer;	
+		this.threadList = threadList;
 	}
 	
 	public void run(){
-		while(running)
-		{
-			//if there is a message
-			//extract message
-			//for each thread
-			//send the message
-		}
+		try{
+			while(running)
+			{
+				String message = buffer.remove();
+				for(ServerThread thread :threadList)
+					thread.write(message);
+				//if there is a message
+				//extract message
+				//for each thread
+				//send the message
+			}
+		}catch(InterruptedException e) {System.out.println("Error running consumer");}
 	}
+}
 
 public class ChatServer
 {
@@ -72,13 +112,18 @@ public class ChatServer
 		int portNumber = 4444; //temporary port number til I figure shit out
 		boolean running = true;
 		
-		Buffer buffer = new Buffer();
+		Buffer buffer = new Buffer(10);
+		ArrayList<ServerThread> threadList = new ArrayList<>();
 		
-		ServerSocket server = new ServerSocket(portNumber)
+		Thread consumer = new Thread(new Consumer(buffer, threadList)).start();
+		
+		ServerSocket server = new ServerSocket(portNumber);
 		while(running)
 		{
-			Socket clientSocket = serverSocket.accept();
-			new Thread(new ServerThread(clientSocket, buffer)).start();
+			Socket clientSocket = server.accept();
+			threadList.add(new Thread(new ServerThread(clientSocket, buffer)).start());
 		}
+		
+		//use an arraylist
 	}
 }
